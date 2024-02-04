f23c9dc6d78fc95a65bdc8b2cbd06ca649b7bc5f
Ravi kumar <ravisingh231218@gmail.com>
2024-02-05 00:27:11 +0530

    Add project files.

---
+++ CRUDWithADONet/DAL/Employee_DAL.cs
@@ -0,0 +1,49 @@
+﻿using CRUDWithADONet.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUDWithADONet.DAL
+{
+    public class Employee_DAL
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+
+        public static IConfiguration Configuration { get; set; }
+
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json");
+
+            Configuration = builder.Build();
+            return Configuration.GetConnectionString("DefaultConnection");
+        }
+        public List<Employee> GetAll()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[usp_Get_Employee]";
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.Id = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]).Date;
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["salary"]);  
+                    employeeList.Add(employee);
+                }
+                _connection.Close();
+            }
+            return employeeList;
+        }
+    }
+}
